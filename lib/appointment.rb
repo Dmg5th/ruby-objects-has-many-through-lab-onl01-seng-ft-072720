6b74968d08c985cbdb2fbce44c815f57398eb35a
Daniel Gould <dmgould310@gmail.com>
2020-08-12 13:38:07 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,18 @@
 class Appointment 
   attr_accessor :date, :patient, :doctor 
   
-  def initialize
+  @@all = []
   
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient 
+    @doctor = doctor
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
   
   
   
