4dbb72d80b2448057ebfe1c0b4729ef8cd82bfa0
Daniel Gould <dmgould310@gmail.com>
2020-08-12 13:02:51 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,6 +11,13 @@ class Genre
     def self.all 
       @@all 
     end 
+    
+    def songs 
+      Song.all.select { |songs| songs.genre == self }
+    end 
+    
+    def artists
+      self.all.select { |genres| genres.artist ==
 
 
 
