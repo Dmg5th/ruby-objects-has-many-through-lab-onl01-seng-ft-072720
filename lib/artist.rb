e5c357a49c51eda41b891f6b41416d2c9e425743
Daniel Gould <dmgould310@gmail.com>
2020-08-12 12:56:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,8 @@ class Artist
     end 
     
     def new_song(name, genre)
-      Song.new(name,)
+      Song.new(name, self, genre)
+    end 
     
     def songs 
       Song.all.select { |songs| songs.artist == self }
