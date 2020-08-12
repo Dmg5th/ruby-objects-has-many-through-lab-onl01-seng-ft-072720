64be56796a25a353a932c3fccfed7bcc7313a8b7
Daniel Gould <dmgould310@gmail.com>
2020-08-12 13:14:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,35 +1,23 @@
-require 'pry'
-class Artist 
-
-    attr_accessor :name, :song
-
-    @@all = []
-
-    def initialize(name_arg)
-        @name = name_arg
-        @@all << self
+class Artist  
+    attr_accessor :name, :song  
+    
+    @@all = []  
+    
+    def initialize(name)
+      @name = name   
+      @@all << self 
     end 
-
-
-    def self.all
-        @@all
+  
+    def self.all 
+      @@all 
+    end 
+    
+    def new_song(name, genre)
+      Song.new(name, self, genre)
     end 
     
     def songs 
-        Song.all.select do |song|
-            song.artist == self
-        end 
-        # binding.pry
+      Song.all.select { |songs| songs.artist == self }
     end 
-
-    def new_song(name, genre)
-        Song.new(name, self, genre)
-    end 
-
-    def genres
-        Song.all.map do |song|
-            song.genre
-        end
-    end 
-
+  
 end 
